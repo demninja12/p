87b214f00b8acd0a32e2e2ed1eab21ab7c4b8f5b
demninja12 <demninja12@gmail.com>
2025-03-04 18:15:04 -0600

    st.save

--- YouTube.py
+++ YouTube.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import webbrowser
 
 st.set_page_config(page_title='YouTube Pig',page_icon='🐷')
 
@@ -20,53 +21,61 @@ if menu == 'Video Categories':
             st.image('https://i.ytimg.com/vi/iAoZFghpRzY/maxresdefault.jpg')
             st.write('Roblox Video Game Tycoon')
             st.success('23:56')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=iAoZFghpRzY')
+            if st.button(label='Play Video',key='1'):
+                webbrowser.open('https://www.youtube.com/watch?v=iAoZFghpRzY')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/X5hsmqElIME/maxresdefault.jpg')
             st.write('Roblox Piggy Book 1')
             st.success('4:53:55')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=X5hsmqElIME')
+            if st.button(label='Play Video',key='2'):
+                webbrowser.open('https://www.youtube.com/watch?v=X5hsmqElIME')
         
         with gm2:
             st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvMc0wZJklMriLs_P3jPqeU9hjY2KIN_TfHQ&s')
             st.write('Minecraft Gameplay Walkthrough Full Game')
             st.success('11:40:04')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=mxHIFijxTZY')
+            if st.button(label='Play Video',key='3'):
+                webbrowser.open('https://www.youtube.com/watch?v=mxHIFijxTZY')
 
             st.write('')
 
             st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmIj4APxj-Eb_WanUAjhG0PbHDCjkuQAfelw&s')
             st.write('Roblox Piggy Book 2')
             st.success('2:05:35')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=Qil2bO3zG34')
+            if st.button(label='Play Video',key='4'):
+                webbrowser.open('https://www.youtube.com/watch?v=Qil2bO3zG34')
         
         with gm3:
             st.image('https://i.ytimg.com/vi/lbk3Yie7_0I/maxresdefault.jpg')
             st.write('Fortnite is MEGA (Season Reaction)')
             st.success('15:38')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=lbk3Yie7_0I')
+            if st.button(label='Play Video',key='5'):
+                webbrowser.open('https://www.youtube.com/watch?v=lbk3Yie7_0I')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/58FeoJdNLKo/maxresdefault.jpg')
             st.write('Minecraft 100 Days')
             st.success('18:02')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=58FeoJdNLKo')
+            if st.button(label='Play Video',key='6'):
+                webbrowser.open('https://www.youtube.com/watch?v=58FeoJdNLKo')
         
         with gm4:
             st.image('https://i.ytimg.com/vi/tBfeIXLuOYg/maxresdefault.jpg')
             st.write('Minecraft But Every 10 Seconds TNT Spawns')
             st.success('23:46')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=tBfeIXLuOYg')
+            if st.button(label='Play Video',key='7'):
+                webbrowser.open('https://www.youtube.com/watch?v=tBfeIXLuOYg')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/yDt87mPmBlQ/maxresdefault.jpg')
             st.write('Realistic Minecraft')
             st.success('18:08')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=yDt87mPmBlQ')
+            if st.button(label='Play Video',key='8'):
+                webbrowser.open('https://www.youtube.com/watch?v=yDt87mPmBlQ')
         
         st.subheader('')#spacer
 
@@ -79,52 +88,61 @@ if menu == 'Video Categories':
             st.image('https://i.ytimg.com/vi/Qqj-ImDsYk8/maxresdefault.jpg')
             st.write('Rocket League Story Mode')
             st.success('32:14')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=Qqj-ImDsYk8')
+            if st.button(label='Play Video',key='9'):
+                webbrowser.open('https://www.youtube.com/watch?v=Qqj-ImDsYk8')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/eU6ouVA3dRc/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBmSuVWq0TUrkN1mab6A5CmTjs_lA')
             st.write('Winning Every Rocket League Game in a Row')
             st.success('36:47')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=eU6ouVA3dRc')
+            if st.button(label='Play Video',key='10'):
+                webbrowser.open('https://www.youtube.com/watch?v=eU6ouVA3dRc')
         
         with sp2:
             st.image('https://images2.minutemediacdn.com/image/upload/c_crop,w_3838,h_2158,x_0,y_0/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/images/voltaxMediaLibrary/mmsport/esports_illustrated/01j4hzxrkt5rzvzcrkkn.jpg')
             st.write('EAFC 25 Mobile Gameplay')
             st.success('9:22')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=ZnHrAuK5e60')
+            if st.button(label='Play Video',key='11'):
+                webbrowser.open('https://www.youtube.com/watch?v=ZnHrAuK5e60')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/eC6aM_Eef1U/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLA-mJK86wq7dotffJcTrh_Yh5lK2Q')
             st.write('The FC 25 Gamplay is actually GOOD')
             st.success('10:21')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=eC6aM_Eef1U')
+            if st.button(label='Play Video',key='12'):
+                webbrowser.open('https://www.youtube.com/watch?v=eC6aM_Eef1U')
         
         with sp3:
             st.image('https://cdn.mos.cms.futurecdn.net/rHSFRBAvLiXw2obQr53nh5.jpg')
             st.write('HIGHLIGHTS | Arsenal vs Man City')
             st.success('2:17')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=4-H15-CeFQo')
+            if st.button(label='Play Video',key='13'):
+                webbrowser.open('https://www.youtube.com/watch?v=4-H15-CeFQo')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/UcnB9e5O5NY/maxresdefault.jpg')
             st.write('The Ultimate Guide for Shooting a Basketball')
             st.success('6:20')
+            if st.button(label='Play Video',key='14'):
+                webbrowser.open('https://www.youtube.com/watch?v=UcnB9e5O5NY')
         
         with sp4:
             st.image('https://i.ytimg.com/vi/XLH9ww29C1E/maxresdefault.jpg')
             st.write('Learn How to Dribble Better in 1 Day')
             st.success('7:55')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=XLH9ww29C1E')
+            if st.button(label='Play Video',key='15'):
+                webbrowser.open('https://www.youtube.com/watch?v=XLH9ww29C1E')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/CMQp0bwjokw/maxresdefault.jpg')
             st.write('How To Dribble A Basketball')
             st.success('9:32')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=CMQp0bwjokw')
+            if st.button(label='Play Video',key='16'):
+                webbrowser.open('https://www.youtube.com/watch?v=CMQp0bwjokw')
         
         st.subheader('')#spacer
 
@@ -137,53 +155,61 @@ if menu == 'Video Categories':
             st.image('https://i.ytimg.com/vi/6iN78oUhB8M/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLC2cC_jge-Isd05IDtzTni3LT0g_g')
             st.write('I Built Bikini Bottom in My Backyard')
             st.success('8:06')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=d748kQzHuRM')
+            if st.button(label='Play Video',key='17'):
+                webbrowser.open('https://www.youtube.com/watch?v=d748kQzHuRM')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/SJN903kCJrc/maxresdefault.jpg')
             st.write('I Built a Secret Gaming Room!')
             st.success('8:59')
-            st.link_button(label='Play Video',url='')
+            if st.button(label='Play Video',key='18'):
+                webbrowser.open('https://www.youtube.com/watch?v=SJN903kCJrc')
         
         with cu2:
             st.image('https://i.ytimg.com/vi/bfONXX3WtT4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD6xvTyIsiv_9Z3nHNqd_3OGMwERQ')
             st.write('I Built 5 SECRET Rooms You’d Never Find!')
             st.success('19:18')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=bfONXX3WtT4')
+            if st.button(label='Play Video',key='19'):
+                webbrowser.open('https://www.youtube.com/watch?v=bfONXX3WtT4')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/llAf-w1WE0c/maxresdefault.jpg')
             st.write('I Built The Krusty Krab in Real Life!')
             st.success('8:41')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=llAf-w1WE0c')
+            if st.button(label='Play Video',key='20'):
+                webbrowser.open('https://www.youtube.com/watch?v=llAf-w1WE0c')
         
         with cu3:
             st.image('https://s1.cdn.autoevolution.com/images/news/gallery/this-luxury-tiny-house-costs-only-25000-and-is-towed-by-a-cybertruck_20.jpg')
             st.write('I Built a Luxury $25,000 Tiny House!')
             st.success('16:57')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=Xwc4VqF6y1U')
+            if st.button(label='Play Video',key='21'):
+                webbrowser.open('https://www.youtube.com/watch?v=Xwc4VqF6y1U')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/4PHGAPMdrAo/maxresdefault.jpg')
             st.write('I Built a Real Life Fortnite Battle Bus!')
             st.success('8:10')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=4PHGAPMdrAo')
+            if st.button(label='Play Video',key='22'):
+                webbrowser.open('https://www.youtube.com/watch?v=4PHGAPMdrAo')
 
         with cu4:
             st.image('https://i.ytimg.com/vi/VQq0gQvKJmM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBiDOvmynBjcSnKgBgGRO_JveuJFA')
             st.write('I Built My House out of Legos')
             st.success('13:33')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=VQq0gQvKJmM')
+            if st.button(label='Play Video',key='23'):
+                webbrowser.open('https://www.youtube.com/watch?v=VQq0gQvKJmM')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/eguZ2NHXWsM/maxresdefault.jpg')
             st.write('I Built MrBeast a Real Life Fortnite Battle Bus!')
             st.success('9:45')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=eguZ2NHXWsM')
+            if st.button(label='Play Video',key='24'):
+                webbrowser.open('https://www.youtube.com/watch?v=eguZ2NHXWsM')
 
         st.subheader('')
         
@@ -197,50 +223,58 @@ if menu == 'Video Categories':
             st.image('https://upload.wikimedia.org/wikipedia/en/thumb/8/86/Peppa_Pig_logo.svg/1200px-Peppa_Pig_logo.svg.png')
             st.write('Peppa Pig')
             st.success('1:04:06')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=Ur--7Y1odjM')
+            if st.button(label='Play Video',key='25'):
+                webbrowser.open('https://www.youtube.com/watch?v=Ur--7Y1odjM')
 
             st.write('')
 
             st.image('https://cdn.shopify.com/s/files/1/0899/1470/files/IMG_1465sm.jpg?v=1678441228')
             st.write('Peppa Pig')
             st.success('2:05:34')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=XHB1w51V02A')
+            if st.button(label='Play Video',key='26'):
+                webbrowser.open('https://www.youtube.com/watch?v=XHB1w51V02A')
         
         with k2:
             st.image('https://images.gem.cbc.ca/v1/synps-cbc/show/perso/cbc_pawpatrol_ott_background_v02.jpg?impolicy=ott&im=Resize=(_Size_)&quality=75')
             st.write('Paw Patrol')
             st.success('12:06')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=z1vXJVI2L5M')
+            if st.button(label='Play Video',key='27'):
+                webbrowser.open('https://www.youtube.com/watch?v=z1vXJVI2L5M')
 
             st.write('')
 
             st.image('https://m.media-amazon.com/images/S/pv-target-images/206a456e12473cd5f5dd92e1e20015eaaded007bd74711ecfdb6937bdfea525d.jpg')
             st.write('Paw Patrol')
             st.success('1:45:59')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=298CQJfZsL8')
+            if st.button(label='Play Video',key='28'):
+                webbrowser.open('https://www.youtube.com/watch?v=298CQJfZsL8')
 
         with k3:
             st.image('https://modernparenting-onemega.com/wp-content/uploads/2024/11/Theres-Another-Blippi-And-Heres-Why-2240x1260.jpg')
             st.write('Blippi')
             st.success('30:24')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=iW4HMPeU3rw')
+            if st.button(label='Play Video',key='29'):
+                webbrowser.open('https://www.youtube.com/watch?v=iW4HMPeU3rw')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/dLPpNMBm_3k/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLABXdqIU4P1vRghjkH1a7jb7C-f4Q')
             st.write('Blippi')
             st.success('2:05:09')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=dLPpNMBm_3k')
+            if st.button(label='Play Video',key='30'):
+                webbrowser.open('https://www.youtube.com/watch?v=dLPpNMBm_3k')
         
         with k4:
             st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAjHGf-acigke5MW6q7orJzVT7PC1eiCEcZg&s')
             st.write('Cocomelon')
             st.success('47:28')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=UCFxJIYgCtQ')
+            if st.button(label='Play Video',key='31'):
+                webbrowser.open('https://www.youtube.com/watch?v=UCFxJIYgCtQ')
 
             st.write('')
 
             st.image('https://i.ytimg.com/vi/oGspaEENWRM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAxbH6SY7oZU_DCRqvZ523ZZpBtzg')
             st.write('Cocomelon')
             st.success('2:00:58')
-            st.link_button(label='Play Video',url='https://www.youtube.com/watch?v=1zU3fEMSOVo')
+            if st.button(label='Play Video',key='32'):
+                webbrowser.open('https://www.youtube.com/watch?v=1zU3fEMSOVo')
